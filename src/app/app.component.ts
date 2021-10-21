b115a52a7f2e992b3dca67f2f96ad82f4f78b09d
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2021-10-21 20:41:26 -0300

    feito botões e algumas funções básicas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Todo } from './../models/todo.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,18 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = []; // []
+  public todos: Todo[] = []; // []
   public title: string = 'Minhas tarefas'
   /**
    *
    */
   constructor() {
-    this.todos.push('passear com o cachorro');
-    this.todos.push('estudar angular');
-    this.todos.push('jogar mir4');
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'ir ao supermercado', true));
+    this.todos.push(new Todo(3, 'jogar mir4', true));
   }
 
   alteraTexto(){
     this.title = 'Teste';
   }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if (index !== -1){
+      this.todos.splice(index,1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
+
+
+
+
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,11 @@
+export class Todo {
+    
+    /**
+     *
+     */
+    constructor(public id: number,
+                public todo: string,
+                public done: boolean) {    }
+    
+
+}
